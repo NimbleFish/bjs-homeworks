cd30d161e203050b8e4f45250b82558ddd559582
NimbleFish <karpov2001ura@mail.ru>
2020-01-16 18:04:38 +0300

    first task ready

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// Задача №1
+let algebra, geography, physics;
+
+algebra = 4;
+geography = 2;
+physics = 5;
+
+// Использую мето библиотеку Math - round -коругляет до ближайшего числа
+let math = Math.round((algebra + geography + physics) / 3);
+console.log(math);
+
+// Задача №2
+
+
+
+// Задача №3
