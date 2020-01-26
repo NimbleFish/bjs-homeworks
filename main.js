e9b8e7253a62df8217fed00962afc71ccd2e9134
NimbleFish <karpov2001ura@mail.ru>
2020-01-26 19:02:15 +0300

    1.1-variables

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// Задача №1
+let algebra, geography, physics;
+
+algebra = 4;
+geography = 2;
+physics = 5;
+
+// Использую мето библиотеку Math - round -коругляет до ближайшего числа
+let math = Math.round((algebra + geography + physics) / 3);
+console.log(math);
+
+// Задача №2
+let myName = "Юра", message;
+message = "Привет, мир! Меня зовут " + myName + "!";
+console.log(message);
+
+// Задача №3
+let x = 2, y = 22, z = 0, result;
+
+result = x*y + 5*z + x-1;
+console.log(result);
