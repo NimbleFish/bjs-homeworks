43e56129026bb59b03f1e3396af607cebef6d13f
NimbleFish <karpov2001ura@mail.ru>
2020-01-27 13:39:25 +0300

    tasks ready

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,86 +1,153 @@
 "use strict"
-
-//Задача №1
-console.log(getResult(2, 4, 3));
-
-function getResult(a, b, c) {
-  b = Math.pow(b, 2);
-  let number = 'Задача №1:', result = [], x1, x2, h = 2 * a, d = b - 4 * (a * c);
-
-  if ( a != 0) {
-    if (d > 0) {
-      console.log(number + 'Есть 2 кореня!');
-      d = Math.sqrt(d);
-      x1 = ((-b) + d) / h;
-      x2 = ((-b) - d) / h;
-      return result = [x1, x2];
-    } else if (d == 0) {
-      console.log(number + 'Есть 1 корень!');
-      x1 = (-b) / h;
-      return result = [x1];
-    } else if (d < 0) {
-      console.log(number + 'Нет корней!')
-      return result;
-    }
+function getResult(a,b,c) {
+  let x = [], x1, x2, d = Math.pow(b, 2) - 4 * (a * c);
+  if ( a === 0) {
+    console.log('Это не квадратное уравнение!');
+    console.log('Есть 1 корень!');
+    x1 = (-b) / (2 * a);
+    x = [x1];
+    return x;
+  }
+  if (d > 0) {
+    console.log('Есть 2 кореня!');
+    d = Math.sqrt(d);
+    x1 = ((-b) + d) / (2 * a);
+    x2 = ((-b) - d) / (2 * a);
+    x = [x1, x2];
+    return x;
+  } else if (d == 0) {
+    console.log('Есть 1 корень!');
+    x1 = (-b) / (2 * a);
+    x = [x1];
+    return x;
+  } else {
+    console.log('Нет корней!')
+    return x;
   }
 }
-console.log('\n');
-
-//Задача №2
-let marks = prompt('Введите оценки'), result, sum;
-marks = marks.split('');
-console.log('Задача №2: ' + getAverageMark(marks));
 
 function getAverageMark(marks) {
+  let sum, averageMark;
   if (marks != '') {
+    console.log(marks);
     if (marks.length >= 5) {
       marks = marks.slice(0, 5);
     }
-    marks[0] = parseInt(marks[0]);
-    marks[1] = parseInt(marks[1]);
-    marks[2] = parseInt(marks[2]);
-    marks[3] = parseInt(marks[3]);
-    marks[4] = parseInt(marks[4]);
-    sum = marks[0] + marks[1] + marks[2] + marks[3] + marks[4];
-    result = sum / marks.length;
+    marks.forEach(function(markCount){
+      marks[markCount] = parseInt(marks[markCount]);
+      sum = marks[markCount];
+    });
+
+    averageMark = sum / marks.length;
   }
-  return result;
+  return averageMark;
 }
-console.log('\n');
 
-//Задача №3
-let name = prompt('Введите ваше имя'), dateOfBirthday = prompt('Введите дату вашего рождения (Пример: 01.01.2000)').split('.'), currentAge;
-console.log('Задача №3: \n' + askDrink(name, dateOfBirthday));
-
-function output(old) {
-  result = '';
+function output(name, old) {
+  let resulted = '';
   if (old) {
-    result = 'Не желаете ли олд-фэшн, ' + name + ' ? ';
+    resulted = 'Не желаете ли олд-фэшн, ' + name + '?';
   }
   else {
-    result = 'Сожалею, ' + name + ', но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот! '
+    resulted = 'Сожалею, ' + name + ', но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот! '
   }
-  return result;
+  return resulted;
 }
 
-function askDrink(name, age) {
-  let date = new Date(), day = date.getDate(), year = date.getFullYear();
-  age[0] = parseInt(age[0]);
-  age[1] = parseInt(age[1]);
-  age[2] = parseInt(age[2]);
-  if (age[2] - year >= 18) {
-    if (age[0] <= day) {
-      console.log('Вам больше 18 лет!');
-      output(true);
-    }
-    else {
-      console.log('Вам меньше 18 лет!');
-      output(false);
-    }
+function askDrink(name, dateOfBirthday){
+  let date = new Date(), year = date.getFullYear(), result;
+  if (year - dateOfBirthday.getFullYear() >= 18) {
+    console.log('Вам больше 18 лет!');
+    result = output(name, true);
   }
   else {
     console.log('Вам меньше 18 лет!');
-    output(false);
+    result = output(name, false);
   }
+  console.log(result)
   return result;
 }
+
+// //Задача №1
+// console.log(getResult(2, 4, 3));
+//
+// function getResult(a, b, c) {
+//   let number = 'Задача №1:', result = [], x1, x2, d = Math.pow(b, 2) - 4 * (a * c);
+//
+//   if ( a === 0) {
+//     console.log('Это не квадратное уравнение!');
+//     console.log(number + 'Есть 1 корень!');
+//     x1 = (-b) / (2 * a);
+//     result = [x1];
+//     return result;
+//   }
+//   if (d > 0) {
+//     console.log(number + 'Есть 2 кореня!');
+//     d = Math.sqrt(d);
+//     x1 = ((-b) + d) / (2 * a);
+//     x2 = ((-b) - d) / (2 * a);
+//     result = [x1, x2];
+//     return result;
+//   } else if (d == 0) {
+//     console.log(number + 'Есть 1 корень!');
+//     x1 = (-b) / (2 * a);
+//     result = [x1];
+//     return result;
+//   } else {
+//     console.log(number + 'Нет корней!')
+//     return result;
+//   }
+// }
+// console.log('\n');
+//
+// //Задача №2
+// let marks = prompt('Введите оценки'), result, sum;
+// marks = marks.split('');
+// console.log('Задача №2: ' + getAverageMark(marks));
+//
+// function getAverageMark(marks) {
+//   if (marks != '') {
+//     if (marks.length >= 5) {
+//       marks = marks.slice(0, 5);
+//     }
+//
+//     marks.forEach(function(markCount){
+//       marks[markCount] = parseInt(marks[markCount]);
+//       sum = marks[markCount];
+//     });
+//
+//     result = sum / marks.length;
+//   }
+//   return result;
+// }
+// console.log('\n');
+//
+// //Задача №3
+// let name = prompt('Введите ваше имя'), dateOfBirthday = prompt('Введите год вашего рождения (Пример: 01.01.2000)').split('.'), currentAge;
+//
+// function output(old) {
+//   let resulted = '';
+//   if (old) {
+//     resulted = 'Не желаете ли олд-фэшн, ' + name + '?';
+//   }
+//   else {
+//     resulted = 'Сожалею, ' + name + ', но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот! '
+//   }
+//   return resulted;
+// }
+//
+// function askDrink(name, dateOfBirthday) {
+//   let date = new Date(), year = date.getFullYear(), resulted;
+//
+//   if (year - dateOfBirthday >= 18) {
+//     console.log('Вам больше 18 лет!');
+//     output(true);
+//   }
+//   else {
+//     console.log('Вам меньше 18 лет!');
+//     output(false);
+//   }
+//   return resulted;
+// }
+//
+// console.log('Задача №3: \n' + askDrink(name, dateOfBirthday));
