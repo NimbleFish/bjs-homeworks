c40c457bf1ba9294df6949302219777b8e1e7748
NimbleFish <karpov2001ura@mail.ru>
2020-02-12 18:47:41 +0300

    complete task 2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -2,15 +2,19 @@ function getAnimalSound(animal) {
     if (animal == undefined) {
       return null;
     }
-
     let sound = animal.sound;
-
     return sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let avarage = 0, roundedAverage;
+
+    for (let i=0; i<marks.length; i++) {
+      marks[i] = parseInt(marks[i]);
+      avarage += marks[i];
+    }
+    avarage = avarage / marks.length;
+    return roundedAverage = Math.round(avarage);
 }
 
 function checkBirthday(birthday) {
