01eed64edb6b179352e88d34f895ad68dfa5c3a9
NimbleFish <karpov2001ura@mail.ru>
2020-02-13 17:23:06 +0300

    ready task 1

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,13 +1,51 @@
-class weapon (name, attack, durability, range) {
+class Weapon {
+  constructor(name, attack, durability, range) {
+    this.name = name;
+    this.attack = attack;
+    this.durability = durability;
+    this.mindDurability = durability;
+    this.range = range;
+  }
+
   takeDamage(damage) {
-
+    this.durability = this.durability - damage;
+    if (this.durability < 0) {
+      this.durability = 0;
+    }
+    return this.durability;
   }
 
-  getDamage(attack) {
-
+  getDamage() {
+    if (this.durability == 0 || this.durability == null || this.durability == undefined || isNaN(this.durability) == true) {
+      return 0;
+    }
+    if (this.durability == Infinity) {
+      return this.attack;
+    }
+    if (this.durability >= (0.3 * this.mindDurability)) {
+      return this.attack;
+    }
+    else {
+      return this.attack/2;
+    }
   }
 
-  isBroken(durability) {
-    
+  isBroken() {
+    if (this.durability == 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
+
+const hand = new Weapon('Рука', 1, Infinity, 1);
+const bow = new Weapon('Лук', 10, 200, 3);
+const sword = new Weapon('Меч', 25, 500, 1);
+const knife = new Weapon('Нож', 5, 300, 1);
+const staff = new Weapon('Посох', 8, 300, 2);
+
+const super_longBow = new Weapon('Длинный лук', 15, 200, 4);
+const super_ax = new Weapon('Секира', 27, 800, 1);
+const super_straff = new Weapon('Посох Бури', 10, 300, 2);
