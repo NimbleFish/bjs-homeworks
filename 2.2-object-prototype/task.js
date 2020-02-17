bc442418ef058e2cb23315dd6e8b4678fb175d24
NimbleFish <karpov2001ura@mail.ru>
2020-02-17 16:10:21 +0300

    no results, task 3 np complete

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -20,27 +20,9 @@ function getAverageMark(marks) {
 }
 
 function checkBirthday(birthday) {
-    birthday = birthday.split('-');
-    window.h = birthday[0];
-    window.m = birthday[2];
-    let count=0, age=0, nStanadrtYear=31622400000, standYear=31536000000, currentYear=0,  date = new Date(),
+    let count=0, age=0, nStanadrtYear=31622400000, standYear=31536000000, currentYear=0,
+    date = new Date(),
     now = `${date.getDay()} ${date.getMonth()+1} ${date.getFullYear()}`, diff=0;
-    birthday[0] = m; birthday[2] = h; delete(m); delete(h);
-    age = date.getFullYear() - parseInt(birthday[2]);
-    // for (let k=0; k<age; k++) {
-    //   if (parseInt(birthday[2])%4 == 0) {
-    //     //Считыет высокосный год
-    //     currentYear = currentYear + nStanadrtYear;
-    //   }
-    //   else {
-    //     currentYear = currentYear + standYear;
-    //   }
-    // }
-    console.log(age);
-    if (age >= 18) {
-      return true;
-    }
-    else {
-      return false;
-    }
+    const data = new Date(birthday), birthdayMs = +date;
+    console.log(Date.now() - birthdayMs);
 }
