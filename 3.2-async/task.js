2e361d3e69555a01fb8cc31e35082a28062a5b39
NimbleFish <karpov2001ura@mail.ru>
2020-02-18 17:30:20 +0300

    check id

--- 3.2-async/task.js
+++ 3.2-async/task.js
@@ -1,1 +1,49 @@
-//Code
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.timerId;
+  }
+
+  addClock(id, time, callback) {
+    let allow = true;
+    if(id == '' && id == undefined || id == null || id == Infinity || isNaN(id) == true) {
+      console.error('Нет id!');
+      allow = false;
+    }
+
+    this.alarmCollection.forEach((i, el)=>{
+      if(i.id == id) {
+        console.error('Нашёл! id: ' + i.id + ' == ' + id);
+        allow = false;
+      }
+    });
+
+    if (allow == true) {
+      this.alarmCollection.push({ 'id':id, 'time':time, 'callback':callback });
+    }
+  }
+
+  removeClock() {
+
+  }
+
+  getCurrentFormattedTime() {
+
+  }
+
+  start() {
+
+  }
+
+  stop() {
+
+  }
+
+  printAlarms() {
+
+  }
+
+  clearAlarms() {
+
+  }
+}
