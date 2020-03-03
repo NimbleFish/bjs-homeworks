e35fdf5b3ac93ebd906e4f6fbf7032d064a97e0f
NimbleFish <karpov2001ura@mail.ru>
2020-03-03 11:21:47 +0300

    ready hard difficult 1

--- Basic JS WEB/js-features/countdown/task.js
+++ Basic JS WEB/js-features/countdown/task.js
@@ -1,9 +1,38 @@
-const time = document.getElementById('timer');
+const hour = document.getElementById('timerHours');
+const min = document.getElementById('timerMinutes');
+const sec = document.getElementById('timerSeconds');
+
+const hour_zero = document.getElementById('timerHours_Zero');
+const min_zero = document.getElementById('timerMinutes_Zero');
+const sec_zero = document.getElementById('timerSeconds_Zero');
 
 function timer() {
-  time.textContent -= 1;
+  if(min.textContent == '0' && hour.textContent > '0') {
+    hour.textContent -= 1;
+    min.textContent = '59';
+    if(hour.textContent < 10) {
+      hour_zero.style.display = 'inline';
+    }
+  }
 
-  if (time.textContent == 0) {
+  if(sec.textContent <= '0' && min.textContent > '0') {
+    min.textContent -= 1;
+    sec.textContent = '59';
+    if((min.textContent == '0' && hour.textContent == '0') || min.textContent < 10) {
+      min_zero.style.display = 'inline';
+    } else {
+      min_zero.style.display = 'none';
+    }
+  }
+  sec.textContent -= 1;
+
+  if(sec.textContent < 10) {
+    sec_zero.style.display = 'inline';
+  } else {
+    sec_zero.style.display = 'none';
+  }
+
+  if (hour.textContent == '0' && min.textContent == '0' && sec.textContent <= '0') {
     clearInterval(timerID);
     window.alert('Вы победили в конкурсе!');
   }
