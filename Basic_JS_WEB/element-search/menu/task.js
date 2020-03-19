286d78faf75f50ea037a0c9fb32db0caf70bbeb0
NimbleFish <karpov2001ura@mail.ru>
2020-03-19 12:14:36 +0300

    end fix menu

--- Basic_JS_WEB/element-search/menu/task.js
+++ Basic_JS_WEB/element-search/menu/task.js
@@ -9,6 +9,25 @@ for(let i = 0; i < menu.childElementCount; i++) {
       } else {
         menu.children[i].children[1].classList.remove('menu_active');
       }
+      if(menu.querySelectorAll('ul .menu_active').length > 1) {
+        let countMenu = checkMenu();
+        for (let g = 0; g <= countMenu; g++) {
+          if(menu.children[g].childElementCount > 1) {
+            menu.children[g].children[1].classList.remove('menu_active');
+          }
+        }
+        menu.children[i].children[1].classList.add('menu_active');
+      }
     });
   }
+}
+
+function checkMenu() {
+  let result;
+  for (let i = 0; i < menu.childElementCount; i++) {
+    if(menu.children[i].childElementCount > 1) {
+      result = i;
+    }
+  }
+  return result;
 }
