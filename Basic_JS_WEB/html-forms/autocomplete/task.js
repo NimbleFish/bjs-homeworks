272e6eb7839e51d751ae0c02986ebbb9abfdc8bc
NimbleFish <karpov2001ura@mail.ru>
2020-03-24 15:30:29 +0300

    task ready

--- Basic_JS_WEB/html-forms/autocomplete/task.js
+++ Basic_JS_WEB/html-forms/autocomplete/task.js
@@ -68,24 +68,18 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    let array = [];
+
+    for (let i = 0; i < this.input.options.length; i++) {
+      if(this.input.options[i].innerText.includes(text)) {
+        array.push({
+           'text' : this.input.options[i].innerText,
+           'value' : i,
+        });
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    }
+
+    return array;
   }
 }
 
