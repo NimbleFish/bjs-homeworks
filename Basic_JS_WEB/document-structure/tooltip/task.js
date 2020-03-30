44a59db1800a32de9f4f65309c2c51961b195fa9
NimbleFish <karpov2001ura@mail.ru>
2020-03-30 12:16:52 +0300

    fix errors and bugs

--- Basic_JS_WEB/document-structure/tooltip/task.js
+++ Basic_JS_WEB/document-structure/tooltip/task.js
@@ -3,11 +3,24 @@ for (var i = 0; i < document.getElementsByTagName('a').length; i++) {
     e.preventDefault();
     for (let f = 0; f < document.getElementsByTagName('a').length; f++) {
       if (document.getElementsByTagName('a')[f] == e.toElement) {
-        document.getElementsByTagName('a')[f].insertAdjacentHTML('afterEnd', `
-        <div class="tooltip" style="left: ${e.toElement.offsetLeft}px; top: ${e.toElement.offsetTop + 20}px; display: block;">
-          ${e.toElement.getAttribute('title')}
-        </div>
-        `);
+        let sosed = document.getElementsByTagName('a')[f].nextElementSibling;
+        if (sosed == null) {
+          document.getElementsByTagName('a')[f].insertAdjacentHTML('afterEnd', `
+          <div class="tooltip" style="left: ${e.toElement.offsetLeft}px; top: ${e.toElement.offsetTop + 20}px; display: block;">
+            ${e.toElement.getAttribute('title')}
+          </div>
+          `);
+        }
+        else if (sosed.getAttribute('class') == 'tooltip') {
+          sosed.remove();
+        }
+        else {
+          document.getElementsByTagName('a')[f].insertAdjacentHTML('afterEnd', `
+          <div class="tooltip" style="left: ${e.toElement.offsetLeft}px; top: ${e.toElement.offsetTop + 20}px; display: block;">
+            ${e.toElement.getAttribute('title')}
+          </div>
+          `);
+        }
       }
     }
   });
