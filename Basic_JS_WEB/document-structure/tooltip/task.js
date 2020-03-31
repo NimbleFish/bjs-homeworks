6b457b6e3f844a5503a65f7685c36b4494c8acd2
NimbleFish <karpov2001ura@mail.ru>
2020-03-31 15:03:53 +0300

    fix

--- Basic_JS_WEB/document-structure/tooltip/task.js
+++ Basic_JS_WEB/document-structure/tooltip/task.js
@@ -1,26 +1,24 @@
-for (var i = 0; i < document.getElementsByTagName('a').length; i++) {
+for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
   document.getElementsByTagName('a')[i].addEventListener('click', (e) => {
     e.preventDefault();
-    for (let f = 0; f < document.getElementsByTagName('a').length; f++) {
-      if (document.getElementsByTagName('a')[f] == e.toElement) {
-        let sosed = document.getElementsByTagName('a')[f].nextElementSibling;
-        if (sosed == null) {
-          document.getElementsByTagName('a')[f].insertAdjacentHTML('afterEnd', `
-          <div class="tooltip" style="left: ${e.toElement.offsetLeft}px; top: ${e.toElement.offsetTop + 20}px; display: block;">
-            ${e.toElement.getAttribute('title')}
-          </div>
-          `);
-        }
-        else if (sosed.getAttribute('class') == 'tooltip') {
-          sosed.remove();
-        }
-        else {
-          document.getElementsByTagName('a')[f].insertAdjacentHTML('afterEnd', `
-          <div class="tooltip" style="left: ${e.toElement.offsetLeft}px; top: ${e.toElement.offsetTop + 20}px; display: block;">
-            ${e.toElement.getAttribute('title')}
-          </div>
-          `);
-        }
+    if (document.getElementsByTagName('a')[i] == e.toElement) {
+      let neighbour = document.getElementsByTagName('a')[i].nextElementSibling;
+      if (neighbour == null) {
+        document.getElementsByTagName('a')[i].insertAdjacentHTML('afterEnd', `
+        <div class="tooltip" style="left: ${e.toElement.offsetLeft}px; top: ${e.toElement.offsetTop + 20}px; display: block;">
+          ${e.toElement.getAttribute('title')}
+        </div>
+        `);
+      }
+      else if (neighbour.getAttribute('class') == 'tooltip') {
+        neighbour.remove();
+      }
+      else {
+        document.getElementsByTagName('a')[i].insertAdjacentHTML('afterEnd', `
+        <div class="tooltip" style="left: ${e.toElement.offsetLeft}px; top: ${e.toElement.offsetTop + 20}px; display: block;">
+          ${e.toElement.getAttribute('title')}
+        </div>
+        `);
       }
     }
   });
