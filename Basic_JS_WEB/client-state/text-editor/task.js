001a268a04f9e9d365524f29901ec291534f827f
NimbleFish <karpov2001ura@mail.ru>
2020-04-01 12:19:41 +0300

    fix

--- Basic_JS_WEB/client-state/text-editor/task.js
+++ Basic_JS_WEB/client-state/text-editor/task.js
@@ -1,10 +1,9 @@
 if (localStorage.text !== undefined) {
   document.getElementById('editor').value = localStorage.text;
 }
-document.getElementById('editor').onchange = () => {
+document.getElementById('editor').addEventListener('keydown', () => {
   localStorage.text = document.getElementById('editor').value;
-}
+});
 document.getElementById('clearEditor').addEventListener('click', ()=>{
   localStorage.clear();
-  document.getElementById('editor').value = '';
 });
