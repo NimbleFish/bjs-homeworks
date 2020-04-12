96bf63f63817b17d5569b89e60f28509ed24f2d3
NimbleFish <karpov2001ura@mail.ru>
2020-04-12 15:58:43 +0300

    optimizate createRequest

--- Basic_JS_WEB/diplom/public/js/api/createRequest.js
+++ Basic_JS_WEB/diplom/public/js/api/createRequest.js
@@ -1,29 +1,13 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
+// Основная функция для совершения запросов на сервер.
 const createRequest = (options = {}) => {
-  const xhr = new XMLHttpRequest;
-  xhr.withCredentials = true;
-  const formData = new FormData();
+  const xhr = new XMLHttpRequest; xhr.withCredentials = true; const formData = new FormData();
   if(options.method === "GET") {
-    xhr.responseType = ''; options.url += "?";
-    for (let data in options.data) {
-      options.url += `${data}=${options.data[data]}&`;
-    }
+    xhr.responseType = ''; options.url += "?"; for (let data in options.data) { options.url += `${data}=${options.data[data]}&`; }
     options.url = options.url.substring(0, options.url.length-1);
   }
   else {
-    xhr.responseType = options.responseType;
-    for (let data in options.data) {
-      formData.append( `${data}`, `${options.data[data]}` );
-    }
+    xhr.responseType = options.responseType; for (let data in options.data) { formData.append( `${data}`, `${options.data[data]}` ); }
   }
   xhr.open(options.method, options.url);
-  try {
-    options.url.indexOf('?') > -1 ? xhr.send() : xhr.send(formData);
-  } catch(error) {
-    console.error(error);
-  }
-  return xhr;
+  try { options.url.indexOf('?') > -1 ? xhr.send() : xhr.send(formData); } catch(error) { console.error(error); } return xhr;
 };
