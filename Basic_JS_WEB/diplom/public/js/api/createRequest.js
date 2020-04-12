b80fae5c0bb1d99808ee4fd07064493b92a2067e
NimbleFish <karpov2001ura@mail.ru>
2020-04-12 15:54:41 +0300

    ready createRequest

--- Basic_JS_WEB/diplom/public/js/api/createRequest.js
+++ Basic_JS_WEB/diplom/public/js/api/createRequest.js
@@ -4,54 +4,26 @@
  * */
 const createRequest = (options = {}) => {
   const xhr = new XMLHttpRequest;
-  let response;
   xhr.withCredentials = true;
+  const formData = new FormData();
   if(options.method === "GET") {
-    xhr.responseType = '';
-    options.url += "?";
+    xhr.responseType = ''; options.url += "?";
     for (let data in options.data) {
       options.url += `${data}=${options.data[data]}&`;
     }
     options.url = options.url.substring(0, options.url.length-1);
-    xhr.open(options.method, options.url);
-    try {
-      xhr.send();
-    } catch(error) {
-      console.error(error);
-    }
-    xhr.addEventListener('readystatechange', () => {
-      if(xhr.readyState === xhr.DONE) {
-
-      }
-    });
   }
   else {
     xhr.responseType = options.responseType;
-    const formData = new FormData();
     for (let data in options.data) {
       formData.append( `${data}`, `${options.data[data]}` );
     }
-    xhr.open(options.method, options.url);
-    try {
-      xhr.send(formData);
-    } catch(error) {
-      console.error(error);
-    }
-    xhr.addEventListener('readystatechange', () => {
-      if(xhr.readyState === xhr.DONE) {
-          console.log(`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()} xhr.response`);
-      }
-    });
-    return `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()}
-    Завершение работы функции`;
   }
+  xhr.open(options.method, options.url);
+  try {
+    options.url.indexOf('?') > -1 ? xhr.send() : xhr.send(formData);
+  } catch(error) {
+    console.error(error);
+  }
+  return xhr;
 };
-// createRequest({
-//   'url' : 'http://localhost:8000/user/login',
-//   'responseType' : 'json',
-//   'method' : 'POST',
-//   'data' : {
-//     'email': 'test@test.ru',
-//     'password': 'test11111111'
-//   }
-// });
