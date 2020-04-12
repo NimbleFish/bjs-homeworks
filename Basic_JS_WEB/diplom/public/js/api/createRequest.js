a778854a7bd8ab9b23c46d776fecb8bbf18215c8
NimbleFish <karpov2001ura@mail.ru>
2020-04-12 15:03:37 +0300

    ~

--- Basic_JS_WEB/diplom/public/js/api/createRequest.js
+++ Basic_JS_WEB/diplom/public/js/api/createRequest.js
@@ -3,54 +3,50 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-    const xhr = new XMLHttpRequest;
-    xhr.withCredentials = true;
+  const xhr = new XMLHttpRequest;
+  let response;
+  xhr.withCredentials = true;
+  if(options.method === "GET") {
+    xhr.responseType = '';
+    options.url += "?";
+    for (let data in options.data) {
+      options.url += `${data}=${options.data[data]}&`;
+    }
+    options.url = options.url.substring(0, options.url.length-1);
+    xhr.open(options.method, options.url);
+    try {
+      xhr.send();
+    } catch(error) {
+      console.error(error);
+    }
+    xhr.addEventListener('readystatechange', () => {
+      if(xhr.readyState === xhr.DONE) {
 
-    if(options.method === "GET") {
-      xhr.responseType = '';
-      options.url += "?";
-      for (let data in options.data) {
-        options.url += `${data}=${options.data[data]}&`;
-      }
-      options.url = options.url.substring(0, options.url.length-1);
-      xhr.open(options.method, options.url);
-      try {
-        xhr.send();
-      } catch(error) {
-        console.error(error);
-      }
-      xhr.addEventListener('readystatechange', () => {
-        if(xhr.readyState === xhr.DONE) {
-          options.callback = (err, response) => {
-            if(err == null) {
-              return xhr.response;
-            }
-          }
         }
-      });
-    }
-    else {
-      xhr.responseType = options.responseType;
-      const formData = new FormData();
-      for (let data in options.data) {
-        formData.append( `${data}`, `${options.data[data]}` );
       }
-      xhr.open(options.method, options.url);
-      try {
-        xhr.send(formData);
-      } catch(error) {
-        console.error(error);
-      }
-      xhr.addEventListener('readystatechange', () => {
-        options.callback = (err, response) => {
-          if(err == null) {
-            return xhr.response;
-          }
-        }
-      });
+    });
+  }
+  else {
+    xhr.responseType = options.responseType;
+    const formData = new FormData();
+    for (let data in options.data) {
+      formData.append( `${data}`, `${options.data[data]}` );
     }
+    xhr.open(options.method, options.url);
+    try {
+      xhr.send(formData);
+      return callback(xhr);
+    } catch(error) {
+      console.error(error);
+    }
+  }
 };
 
+function callback(xhr) {
+  if(xhr.readyState === xhr.DONE) {
+    return xhr.response;
+  }
+}
 // createRequest({
 //   'url' : 'http://localhost:8000/user/login',
 //   'responseType' : 'json',
