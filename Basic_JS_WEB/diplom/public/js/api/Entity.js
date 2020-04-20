c66cea88ed6183ac10783a6e344db580cb1db139
NimbleFish <karpov2001ura@mail.ru>
2020-04-20 14:25:39 +0300

    error adress

--- Basic_JS_WEB/diplom/public/js/api/Entity.js
+++ Basic_JS_WEB/diplom/public/js/api/Entity.js
@@ -1,42 +1,30 @@
-/**
- * Класс Entity - базовый для взаимодействия с сервером.
- * Имеет свойство URL, равно пустой строке.
- * Имеет свойство HOST, равно 'https://bhj-diplom.letsdocode.ru'.
- * */
-class Entity {
+class Entity { // Базовый калсс для взаимодействия с сервером.
+  static list( data, callback = f => f ) { // Запрашивает с сервера список данных.
+    createRequest({
+      'url' : 'http://localhost:8000/entity/create',
+      'method' : 'POST',
+      'responseType' : 'json',
+      'data' : { 'mail' : data.mail, '_method' : 'PUT' },
+      'callback' : callback
+    });
+  }
 
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static list( data, callback = f => f ) {
+  static create( data, callback = f => f ) { // Создаёт счёт или доход/расход с помощью запроса на сервер.
 
   }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
-  static create( data, callback = f => f ) {
+  static get( id = '', data, callback = f => f ) { // Получает информацию о счёте или доходе/расходе
 
   }
 
-  /**
-   * Получает информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static get( id = '', data, callback = f => f ) {
-
-  }
-
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static remove( id = '', data, callback = f => f ) {
+  static remove( id = '', data, callback = f => f ) { // Удаляет информацию о счёте или доходе/расходе
 
   }
 }
 
+Entity.list({
+  'mail' : 'demo@mail.ru',
+  'password' : 'demo'
+}, (err, res) => {
+  console.log(res);
+});
