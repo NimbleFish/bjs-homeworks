9f3635838c5afb29e6178094a9dc9e118560c305
NimbleFish <karpov2001ura@mail.ru>
2020-04-20 13:58:59 +0300

    mini

--- Basic_JS_WEB/diplom/public/js/api/createRequest.js
+++ Basic_JS_WEB/diplom/public/js/api/createRequest.js
@@ -1,30 +1,15 @@
-// Основная функция для совершения запросов на сервер.
-const createRequest = (options = {}) => {
-  const xhr = new XMLHttpRequest;
+const createRequest = (options = {}) => { // Основная функция для совершения запросов на сервер.
+  const xhr = new XMLHttpRequest, formData = new FormData();
   xhr.withCredentials = true;
-  const formData = new FormData();
   if (options.method === "GET") {
-    xhr.responseType = '';
-    options.url += "?";
-    for (let data in options.data) {
-      options.url += `${data}=${options.data[data]}&`;
-    }
+    xhr.responseType = ''; options.url += "?";
+    for (let data in options.data) { options.url += `${data}=${options.data[data]}&`; }
     options.url = options.url.substring(0, options.url.length - 1);
   } else {
     xhr.responseType = options.responseType;
-    for (let data in options.data) {
-      formData.append(`${data}`, `${options.data[data]}`);
-    }
+    for (let data in options.data) { formData.append(`${data}`, `${options.data[data]}`); }
   }
-  xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === xhr.DONE) {
-      options.callback(xhr.response.error, xhr.response);
-    }
-  });
+  xhr.addEventListener('readystatechange', () => { if (xhr.readyState === xhr.DONE) { options.callback(xhr.response.error, xhr.response); } });
   xhr.open(options.method, options.url);
-  try {
-    options.url.indexOf('?') > -1 ? xhr.send() : xhr.send(formData);
-  } catch (error) {
-    console.error(error);
-  }
+  try { options.url.indexOf('?') > -1 ? xhr.send() : xhr.send(formData); } catch (error) { console.error(error); }
 };
