db8356ef9bd0806df5a4548fd3ebecfa96d5813c
NimbleFish <karpov2001ura@mail.ru>
2020-04-21 13:08:37 +0300

    ready Entity

--- Basic_JS_WEB/diplom/public/js/api/Entity.js
+++ Basic_JS_WEB/diplom/public/js/api/Entity.js
@@ -1,30 +1,71 @@
 class Entity { // Базовый калсс для взаимодействия с сервером.
   static list( data, callback = f => f ) { // Запрашивает с сервера список данных.
     createRequest({
-      'url' : 'http://localhost:8000/entity/create',
-      'method' : 'POST',
+      'url' : 'http://localhost:8000/account/',
+      'method' : 'GET',
       'responseType' : 'json',
-      'data' : { 'mail' : data.mail, '_method' : 'PUT' },
+      'data' : { 'mail' : data.mail, 'password' : data.password },
       'callback' : callback
     });
   }
 
   static create( data, callback = f => f ) { // Создаёт счёт или доход/расход с помощью запроса на сервер.
-
+    createRequest({
+      'url' : 'http://localhost:8000/account/',
+      'method' : 'POST',
+      'responseType' : 'json',
+      'data' : { 'mail' : data.mail, 'password' : data.password, '_method' : 'PUT' },
+      'callback' : callback
+    });
   }
 
   static get( id = '', data, callback = f => f ) { // Получает информацию о счёте или доходе/расходе
-
+    createRequest({
+      'url' : 'http://localhost:8000/account/get',
+      'method' : 'GET',
+      'responseType' : 'json',
+      'data' : { 'id' : id, 'mail' : data.mail, 'password' : data.password },
+      'callback' : callback
+    });
   }
 
   static remove( id = '', data, callback = f => f ) { // Удаляет информацию о счёте или доходе/расходе
-
+    createRequest({
+      'url' : 'http://localhost:8000/account/',
+      'method' : 'POST',
+      'responseType' : 'json',
+      'data' : { 'id' : id, 'mail' : data.mail, 'password' : data.password, '_method' : 'DELETE' },
+      'callback' : callback
+    });
   }
 }
-
-Entity.list({
+// Вызов
+/*
+Entity.create({
   'mail' : 'demo@mail.ru',
   'password' : 'demo'
 }, (err, res) => {
   console.log(res);
 });
+
+Entity.get('1', {
+  'mail' : 'demo@mail.ru',
+  'password' : 'demo'
+}, (err, res) => {
+  console.log(JSON.parse(res));
+});
+
+Entity.list({
+  'mail' : 'demo@mail.ru',
+  'password' : 'demo'
+}, (err, res) => {
+  console.log(JSON.parse(res));
+});
+
+Entity.remove('1', {
+  'mail' : 'demo@mail.ru',
+  'password' : 'demo'
+}, (err, res) => {
+  console.log(res);
+});
+*/
