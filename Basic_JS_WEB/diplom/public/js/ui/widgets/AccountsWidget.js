180933cf25d80fb926471d1190403094dcf9ebdf
NimbleFish <karpov2001ura@mail.ru>
2020-05-10 14:11:15 +0300

    minimaze and work

--- Basic_JS_WEB/diplom/public/js/ui/widgets/AccountsWidget.js
+++ Basic_JS_WEB/diplom/public/js/ui/widgets/AccountsWidget.js
@@ -1,13 +1,4 @@
 class AccountsWidget { // Управляет блоком отображения счетов в боковой колонке
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью
-   * AccountsWidget.registerEvents()
-   * Вызывает AccountsWidget.update() для получения
-   * списка счетов и последующего отображения
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
   constructor( element ) {
     if (element) {
       this.element = element;
@@ -18,59 +9,31 @@ class AccountsWidget { // Управляет блоком отображения
     }
   }
 
-  /**
-   * При нажатии на .create-account открывает окно
-   * #modal-new-account для создания нового счёта
-   * При нажатии на один из существующих счетов
-   * (которые отображены в боковой колонке),
-   * вызывает AccountsWidget.onSelectAccount()
-   * */
   registerEvents() {
     document.getElementsByClassName('create-account')[0].addEventListener('click', (e) => {
       new Modal(App.getModal('createAccount').element).open();
     });
-    let el = document.getElementsByClassName('sidebar-menu accounts-panel')[0].querySelectorAll('account');
+    let el = document.getElementsByClassName('sidebar-menu accounts-panel')[0].querySelectorAll('.account');
     for (let i = 0; i < el.length; i++) {
+      console.log(el[i]);
       el[i].addEventListener('click', () => {
-        this.onSelectAccount();
+        this.onSelectAccount(el[i]);
       });
     }
   }
 
-  /**
-   * Метод доступен только авторизованным пользователям
-   * (User.current()).
-   * Если пользователь авторизован, необходимо
-   * получить список счетов через Account.list(). При
-   * успешном ответе необходимо очистить список ранее
-   * отображённых счетов через AccountsWidget.clear().
-   * Отображает список полученных счетов с помощью
-   * метода renderItem()
-   * */
   update() {
     if(User.current()) {
-      Account.list({
-        'id' : User.current().id,
-        'mail' : User.current().email,
-        'name' : User.current().name
-      }, (err, res) => {
-        console.log(JSON.parse(res));
+      Account.list({ 'id' : User.current().id, 'mail' : User.current().email, 'name' : User.current().name }, (err, res) => {
+        res = JSON.parse(res);
+        if (res.success == true) { this.clear(); this.renderItem(res.data); } else { console.error(err); }
       });
-      // this.clear();
-      // this.renderItem();
     }
   }
 
-  /**
-   * Очищает список ранее отображённых счетов.
-   * Для этого необходимо удалять все элементы .account
-   * в боковой колонке
-   * */
   clear() {
-    let el = document.getElementsByClassName('sidebar-menu accounts-panel')[0].querySelectorAll('account');
-    for (let i = 0; i < el.length; i++) {
-      el[i].remove();
-    }
+    let el = document.getElementsByClassName('sidebar-menu accounts-panel')[0].querySelectorAll('.account');
+    for (let i = 0; i < el.length; i++) { el[i].remove(); }
   }
 
   /**
@@ -81,25 +44,26 @@ class AccountsWidget { // Управляет блоком отображения
    * Вызывает App.showPage( 'transactions', { account_id: id_счёта });
    * */
   onSelectAccount( element ) {
-
+    let el = document.getElementsByClassName('sidebar-menu accounts-panel')[0].querySelectorAll('.account');
+    for (let i = 0; i < el.length; i++) {
+      element.classList.remove('active');
+    }
+    element.classList.add('active');
+    App.showPage('transactions', { 'account_id' : element.dataset.id });
   }
 
-  /**
-   * Возвращает HTML-код счёта для последующего
-   * отображения в боковой колонке.
-   * item - объект с данными о счёте
-   * */
   getAccountHTML( item ) {
-
+    return `<li class="account" data-id="${item.id}">
+        <a href="#">
+            <span>${item.name}</span> /
+            <span>${item.sum} ₽</span>
+        </a>
+     </li>`;
   }
 
-  /**
-   * Получает массив с информацией о счетах.
-   * Отображает полученный с помощью метода
-   * AccountsWidget.getAccountHTML HTML-код элемента
-   * и добавляет его внутрь элемента виджета
-   * */
   renderItem( item ) {
-
+    for (let i = 0; i < item.length; i++) {
+      this.element.insertAdjacentHTML('beforeend', this.getAccountHTML( item[i] ));
+    }
   }
 }
