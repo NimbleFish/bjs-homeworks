c8b32b2852cfaf4643c3d46b01c4a8cae179a2ea
NimbleFish <karpov2001ura@mail.ru>
2020-05-10 14:11:39 +0300

    minimaze

--- Basic_JS_WEB/diplom/public/js/ui/forms/AsyncForm.js
+++ Basic_JS_WEB/diplom/public/js/ui/forms/AsyncForm.js
@@ -1,39 +1,22 @@
-/**
- * Класс AsyncForm управляет всеми формами
- * приложения, которые не должны быть отправлены с
- * перезагрузкой страницы. Вместо этого данные
- * с таких форм собираются и передаются в метод onSubmit
- * для последующей обработки
- * */
-class AsyncForm {
+class AsyncForm { // Управляет всеми формами приложения, которые не должны быть отправлены с перезагрузкой страницы.
   constructor( element ) { // Сохраняет переданный элемент и регистрирует события.
     if (!element) {
       console.error("Передана пустая или несуществующая форма!");
     } else {
+      console.log(element);
       this.element = element;
       this.registerEvents();
     }
   }
 
-  /**
-   * Необходимо запретить отправку формы. В момент отправки
-   * вызывает метод submit()
-   * */
-  registerEvents() {
+  registerEvents() { // В момент отправки вызывает метод submit()
     this.element.addEventListener('submit', (e) => {
       e.preventDefault();
       this.submit();
     });
   }
 
-  /**
-   * Преобразует данные формы в объект вида
-   * {
-   *  'название поля формы 1': 'значение поля формы 1',
-   *  'название поля формы 2': 'значение поля формы 2'
-   * }
-   * */
-  getData() {
+  getData() { // Преобразует данные формы в объект FormData
     let el = document.querySelectorAll(`#${this.element.id} input.form-control`), data = {};
     for (let i = 0; i < el.length; i++) {
       data[el[i].name] = el[i].value;
@@ -45,11 +28,7 @@ class AsyncForm {
 
   }
 
-  /**
-   * Вызывает метод onSubmit и передаёт туда
-   * данные, полученные из метода getData()
-   * */
-  submit() {
-    this.onSubmit({ 'url': this.element.action, 'method': this.element.method,'data' : this.getData()});
+  submit() { // Вызывает метод onSubmit и передаёт данные, полученные из метода getData()
+    this.onSubmit({ 'url': this.element.action, 'method': this.element.method, 'data' : this.getData()});
   }
 }
