ea7472e78c41855080245a000cd038674d8902a4
NimbleFish <karpov2001ura@mail.ru>
2020-05-10 14:11:29 +0300

    complete

--- Basic_JS_WEB/diplom/public/js/ui/forms/CreateAccountForm.js
+++ Basic_JS_WEB/diplom/public/js/ui/forms/CreateAccountForm.js
@@ -1,13 +1,13 @@
 class CreateAccountForm extends AsyncForm { // Управляет формой создания нового счёта
-  /**
-   * Создаёт счёт с помощью Account.create и закрывает
-   * окно (в котором находится форма) в случае успеха,
-   * а также вызывает App.update()
-   * и сбрасывает форму
-   * */
-  onSubmit( options ) {
-    // Account.create();
-
-    // App.update();
+  onSubmit( options ) { // Создаёт счёт с помощью Account.create и закрывает окно а также вызывает App.update() и сбрасывает форму
+    Account.create({
+      'id' : User.current().id,
+      'mail' : User.current().email,
+      'name' : options.data.name
+    }, (err, res) => {
+      new Modal(App.getModal('createAccount').element).close();
+      this.element.querySelectorAll('.form-control')[0].value = '';
+      App.update();
+    });
   }
 }
