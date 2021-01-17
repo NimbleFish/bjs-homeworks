244a5db502470c7ced27d197d8d9613e4526a42e
NimbleFish <karpov2001ura@mail.ru>
2021-01-17 17:49:13 +0300

    store-class ready

---
+++ React/store-class/src/ShopItemClass.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class ShopItemClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {item} = this.props;
+    return (
+      <div class="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div class="description">{item.descriptionFull}</div>
+        <div class="highlight-window mobile"><div class="highlight-overlay" /></div>
+        <div class="divider" />
+        <div class="purchase-info">
+          <div class="price">{item.currency}{item.price}.00</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShopItemClass;
