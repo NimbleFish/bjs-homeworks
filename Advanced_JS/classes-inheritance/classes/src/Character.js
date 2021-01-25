09f39b0ea9b8321f5b09270cceff702d5ab9ae5f
NimbleFish <karpov2001ura@mail.ru>
2021-01-25 12:48:26 +0300

    stage 2 complete

--- Advanced_JS/classes-inheritance/classes/src/Character.js
+++ Advanced_JS/classes-inheritance/classes/src/Character.js
@@ -3,4 +3,15 @@ export default class Character {
     this.attack = attack;
     this.defence = defence;
   }
+
+  levelUp() {
+    if (this.health > 0) {
+      this.level++;
+      this.attack = this.attack + (this.attack * 0.2);
+      this.defence = this.defence + (this.defence * 0.2);
+      this.health = 100;
+    } else {
+      new throw('Нельзя повысить уровень умершего!');
+    }
+  }
 }
