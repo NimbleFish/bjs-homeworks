49d89a56f51df54e6f460db707a38747ead4138a
NimbleFish <karpov2001ura@mail.ru>
2021-02-07 11:57:32 +0300

    restruct jest autotest for ErrorRepository.js

--- Advanced_JS/containers/map/src/js/__tests__/ErrorRepository.test.js
+++ Advanced_JS/containers/map/src/js/__tests__/ErrorRepository.test.js
@@ -1,9 +1,18 @@
 import ErrorRepository from '../ErrorRepository';
 
-test('check error codes', () => {
-  const error = new ErrorRepository();
+const error = new ErrorRepository();
+test('error code(404)', () => {
   expect(error.translate(404)).toEqual('Ничего не найдено');
+});
+
+test('error code(224)', () => {
   expect(error.translate(224)).toEqual('В команде уже есть этот персонаж');
+});
+
+test('error code(196)', () => {
   expect(error.translate(196)).toEqual('Нет такой настройки');
+});
+
+test('error code(234)', () => {
   expect(error.translate(234)).toEqual('Unknown error!');
 });
