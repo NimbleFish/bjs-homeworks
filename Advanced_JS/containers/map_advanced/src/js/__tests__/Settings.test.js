4aae02c473b778f278226d671bed95938c7f65c8
NimbleFish <karpov2001ura@mail.ru>
2021-02-07 11:54:57 +0300

    restruct jest autotest for Settings.js

--- Advanced_JS/containers/map_advanced/src/js/__tests__/Settings.test.js
+++ Advanced_JS/containers/map_advanced/src/js/__tests__/Settings.test.js
@@ -1,14 +1,29 @@
 import Settings from '../Settings';
 
-test('check how work Map*', () => {
-  const settings = new Settings();
-  const res1 = [["theme", "dark"], ["music", "trance"], ["difficulty", "easy"]];
-  const res2 = [["theme", "gray"], ["music", "off"], ["difficulty", "nightmare"]];
-  expect(new Settings().change).toEqual('А хде данные?');
-  expect(settings.toArray(settings.DEFAULT)).toEqual(res1);
-  expect(settings.toArray(new Settings('gray', 'off', 'nightmare').change)).toEqual(res2);
+const settings = new Settings();
+const res1 = [["theme", "dark"], ["music", "trance"], ["difficulty", "easy"]];
+const res2 = [["theme", "gray"], ["music", "off"], ["difficulty", "nightmare"]];
 
+test('check empty data', () => {
+  expect(new Settings().change).toEqual('А хде данные?');
+});
+
+test('check DEFAULT settings', () => {
+  expect(settings.toArray(settings.DEFAULT)).toEqual(res1);
+});
+
+test('test normal preset 1', () => {
+  expect(settings.toArray(new Settings('gray', 'off', 'nightmare').change)).toEqual(res2);
+});
+
+test('test errores preset (theme)', () => {
   expect(new Settings('123', "chillout", "normal").change).toEqual('Нет такой настройки');
+});
+
+test('test errores preset (music)', () => {
   expect(new Settings("light", "PSY", "hard").change).toEqual('Нет такой настройки');
+});
+
+test('test errores preset (difficulty)', () => {
   expect(new Settings("gray", "rock", "???").change).toEqual('Нет такой настройки');
 });
