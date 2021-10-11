57477d181cc25177da20daad8a743feb95afd2a4
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-11 20:02:35 +0300

    typical error named class in React fix

--- React/composition/decomposition/src/components/Weather.js
+++ React/composition/decomposition/src/components/Weather.js
@@ -2,8 +2,8 @@ export default function Weather(props) { // Рендерит блок погод
     return (
         <div className="block">
             <div className="blockTitle">Погода</div>
-            <div class="weatherContainer">
-                <span className="mainWeather"><img src={props.icon} alt="weather_img" /> <span class="mainWeather">+{props.weather.now}</span></span>
+            <div className="weatherContainer">
+                <span className="mainWeather"><img src={props.icon} alt="weather_img" /> <span className="mainWeather">+{props.weather.now}</span></span>
                 <span className="additionalWeather">
                     <div>Утром +{props.weather.morning},</div>
                     <div>днём +{props.weather.day}</div>
