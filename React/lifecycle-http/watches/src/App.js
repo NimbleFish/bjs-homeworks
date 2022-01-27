1b8c0a536acfd4ca0e88df9e4b756921e6cc514c
NimbleFIsh <karpov2001ura@mail.ru>
2022-01-27 15:39:39 +0300

    complete

--- React/lifecycle-http/watches/src/App.js
+++ React/lifecycle-http/watches/src/App.js
@@ -1,54 +1,58 @@
-import React from "react";
+import { useState } from "react";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      'name': '',
-      'date': 0,
-      'clocks': []
-    }
-  }
-  
-  changeValue = frm => this.setState({[frm.target.id]: frm.target.id === 'date' ? parseInt(frm.target.value) : frm.target.value});
-  
-  addClock() {
-    console.log(this.state);
-    // this.state.clocks.push({
-    //   'name': this.form.name,
-    //   'timeZone': this.form.date,
-    //   'time': `${new Date().getUTCHours()+this.form.date}:${new Date().getUTCMinutes()}`
-    // });
+export default function App() {
+  const [ clocks, changeClocks ] = useState([  ]);
+  const [ form, setForm ] = useState({ 'name': '', 'date': 0 });
+
+  const valid = data => data < 10 ? "0"+ data : data;
+  const getTime = zone => `${valid(new Date().getUTCHours()+zone)}:${valid(new Date().getUTCMinutes())}:${valid(new Date().getUTCSeconds())}`;
+  const changeValue = frm => setForm(prevState => ({...prevState, [frm.target.id]: frm.target.id === 'date' ? parseInt(frm.target.value) : frm.target.value}));
+
+  const removeClock = id => {
+    clearInterval(id);
+    changeClocks( prevState => ({ ...prevState, clocks: prevState.clocks.filter(el => el.id !== id) }));
   }
 
-  componentDidMount() {
-    setInterval(() => this.state.clocks.forEach(el => el.time = `${new Date().getUTCHours()+el.timeZone}:${new Date().getUTCMinutes()}`), 1000);
+  const addClock = () => {
+    if (!clocks.filter(el => el.timeZone === form.date)[0]) { // Проверка на существование часов с таким чп
+      const name = form.name; // Дамп данных
+      const timeZone = form.date; // Дамп данных
+      setForm({ name: "", date: 0 }); // Сброс полей ввода
+
+      const getInterval = () => setInterval(() => changeClocks(prevState =>
+          ([ ...prevState.filter(el => el.timeZone !== timeZone), { ...prevState.filter(el => el.timeZone === timeZone)[0], time: getTime(timeZone) } ])), 1000);
+
+      changeClocks(prevState => ([ ...prevState, { name, timeZone, time: getTime(timeZone), id: getInterval() } ]));
+
+    } else alert("Уже существуют часы с этим часовым поясом!");    
   }
 
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="controls-wrapper">
-          <div className="block-wrapper">
-            <label htmlFor="name">Название</label>
-            <input id="name" type="text" value={this.state.form.name} onChange={this.changeValue} />
-          </div>
-          <div className="block-wrapper">
-            <label htmlFor="date">Временная зона</label>
-            <input id="date" type="number" value={this.state.form.date} onChange={this.changeValue} />
-          </div>
-          <div onClick={this.addClock}>Добавить</div>
+  return (
+    <div className="wrapper">
+      <div className="controls-wrapper">
+        <div className="block-wrapper">
+          <label htmlFor="name">Название</label>
+          <input id="name" type="text" value={form.name} onChange={changeValue} />
         </div>
-        <ul>
-          {
-            this.state.clocks.length !== 0 && this.state.clocks.map(el => {
-              <li>
-                {el.time}
+        <div className="block-wrapper">
+          <label htmlFor="date">Временная зона</label>
+          <input id="date" type="number" value={form.date} min="-12" mas="12" onChange={changeValue} />
+        </div>
+        <div className="btn" onClick={addClock}>Добавить</div>
+      </div>
+      <ul>
+        {
+          clocks.length !== 0 && clocks.map(el => {
+            return (
+              <li key={el.id}>
+                <div onClick={() => removeClock(el.id)} className="remove">Удалить</div>
+                <div className="name">{el.name}</div>
+                <div className="time">{el.time}</div>
               </li>
-            })
-          }
-        </ul>
-      </div>    
-    );
-  }
+            );
+          })
+        }
+      </ul>
+    </div>    
+  );
 }
